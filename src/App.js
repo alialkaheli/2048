d574752bc6a8e4b346ca95932794db6ffe23311b
ali alkaheli <ali.alkaheli96@gmail.com>
2019-04-22 21:30:15 -0700

    Create the outline of the board

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
 import React from 'react';
 import './App.css';
+import BlockItem from './blockitem'
 
 class App extends React.Component {
+  constructor(){
+    super()
+    this.state = {
+      board: [
+        [0,0,0,0],
+        [0,0,0,0],
+        [0,0,0,0],
+        [0,0,0,0]
+      ]
+    }
+  }
+
   render(){
+    let board = this.state.board.map(i =>{
+       let row = i.map((item, j) => {return <BlockItem key={j} num={item}></BlockItem>})
+      return <div className="board-row">{row}</div>
+    })
     return (
       <div className="App">
         <h1>2048</h1>
+        {board}
       </div>
     );
   }
