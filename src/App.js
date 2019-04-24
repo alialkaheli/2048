1a76d9a19bb119bdf40618760847dbe63f8dc3ac
ali alkaheli <ali.alkaheli96@gmail.com>
2019-04-23 21:29:26 -0700

    setting up the board with a tile of two random places

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
-import BlockItem from './blockitem'
+import BlockItem from './blockitem';
+import Functionality from './functionality';
 
 class App extends React.Component {
   constructor(){
@@ -13,8 +14,30 @@ class App extends React.Component {
         [0,0,0,0]
       ]
     }
+    this.addNumber = this.addNumber.bind(this)
   }
 
+  addNumber(){
+    let options = []
+    for(let i = 0; i<4;i++){
+      for(let j = 0; j<4; j++){
+        if(this.state.board[i][j] === 0){
+          options.push([i,j])
+        }
+      }
+    }
+    let idx = options[Math.floor(Math.random() * options.length)]
+    let board = [...this.state.board]
+    let item = board[idx[0]][idx[1]]
+    item = 2
+    board[idx[0]][idx[1]] = item
+    this.setState({board})
+  }
+
+  componentDidMount(){
+    this.addNumber()
+    this.addNumber()
+  }
   render(){
     let board = this.state.board.map(i =>{
        let row = i.map((item, j) => {return <BlockItem key={j} num={item}></BlockItem>})
@@ -23,7 +46,10 @@ class App extends React.Component {
     return (
       <div className="App">
         <h1>2048</h1>
+
         {board}
+
+        <Functionality addNumber={this.addNumber} />
       </div>
     );
   }
