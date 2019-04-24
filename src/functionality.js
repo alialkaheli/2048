14e730aa8312ee77f4efb630437e8952e1f0ae6f
ali alkaheli <ali.alkaheli96@gmail.com>
2019-04-23 21:55:23 -0700

    create all four arrows of the board

--- src/functionality.js
+++ src/functionality.js
@@ -1,8 +1,30 @@
 import React from "react";
+import './functionality.css'
 
 class Functionality extends React.Component{
+    constructor(){
+        super()
+    }
+
     render(){
-        return(<h1>hello</h1>)
+        return(
+        <div className="whole-board">
+
+            <div className="updown-board">
+                <button className="updown-button">UP</button>
+            </div>
+
+            <div className="mid-board">
+                <button className="leftright-button"> LEFT </button>
+                <div>{this.props.board}</div>
+                <button className="leftright-button"> RIGHT </button>
+            </div>
+
+            <div className="updown-board">
+                <button className="updown-button">DOWN</button>
+            </div>
+        </div>
+        )
     }
 }
 
