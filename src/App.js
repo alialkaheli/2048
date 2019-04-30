3be006e29a2609708d0c3542e963e168b08bd3b9
ali alkaheli <ali.alkaheli96@gmail.com>
2019-04-29 20:08:02 -0700

    Application functionality modifications

--- src/App.js
+++ src/App.js
@@ -1,57 +1,192 @@
 import React from 'react';
 import './App.css';
-import BlockItem from './blockitem';
 import Functionality from './functionality';
+import { HotKeys } from 'react-hotkeys';
 
 class App extends React.Component {
-  constructor(){
-    super()
+  constructor(props) {
+    super(props)
     this.state = {
-      board: [
-        [0,0,0,0],
-        [0,0,0,0],
-        [0,0,0,0],
-        [0,0,0,0]
-      ]
+      gridData: [[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]],
+      score: 0
     }
-    this.addNumber = this.addNumber.bind(this)
+    this.blankGrid = [[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]]
+    this.leftOp = this.leftOp.bind(this)
+    this.rightOp = this.rightOp.bind(this)
+    this.upOp = this.upOp.bind(this)
+    this.downOp = this.downOp.bind(this)
+
+    this.map = {
+      'moveLeft': 'left',
+      'moveRight': 'right',
+      'moveUp': 'up',
+      'moveDown': 'down'
+    };
+
+
+    this.handlers = {
+      'moveLeft': this.leftOp,
+      'moveRight': this.rightOp,
+      'moveUp': this.upOp,
+      'moveDown': this.downOp
+    }
+  }
+  initGame() {
+    let grid = this.blankGrid.map(row => row.slice())
+    grid = this.addNumber(grid)
+    grid = this.addNumber(grid)
+    this.setState({
+      gridData: grid,
+      score: 0
+    })
+    this.forceUpdate()
+
+
   }
 
-  addNumber(){
-    let options = []
-    for(let i = 0; i<4;i++){
-      for(let j = 0; j<4; j++){
-        if(this.state.board[i][j] === 0){
-          options.push([i,j])
+  addNumber(grid) {
+    const availableSpot = []
+    grid.forEach((rowData, x) =>
+      rowData.forEach((data, y) => {
+        if (!data) availableSpot.push({ x, y })
+      })
+    )
+    const randomSpot = availableSpot[Math.floor(Math.random() * availableSpot.length)]
+    grid[randomSpot.x][randomSpot.y] = Math.random() < 0.2 ? 4 : 2
+    return grid
+  }
+  slide(row) {
+    const newRow = row.filter(data => data)
+    const zerosArr = Array(4 - newRow.length).fill(0)
+    return [...zerosArr, ...newRow]
+  }
+
+  combine(row) {
+    let a, b
+    let score = this.state.score;
+    for (let i = 3; i > 0; i--) {
+      a = row[i]
+      b = row[i - 1]
+      if (a === b) {
+        score += 2 * a
+        row[i] = a + b
+        row[i - 1] = 0
+      }
+    }
+    this.setState({ score })
+    return row
+  }
+
+  slideAndCombine(row) {
+    row = this.slide(row)
+    row = this.combine(row)
+    return row
+  }
+
+  diffGrid(grid) {
+    let isDiff = false
+    for (let i = 0; i < grid.length; i++) {
+      for (let j = 0; j < grid.length; j++) {
+        if (grid[i][j] !== this.state.gridData[i][j]) {
+          isDiff = true
         }
       }
     }
-    let idx = options[Math.floor(Math.random() * options.length)]
-    let board = [...this.state.board]
-    let item = board[idx[0]][idx[1]]
-    item = 2
-    board[idx[0]][idx[1]] = item
-    this.setState({board})
+    if (isDiff) {
+      grid = this.addNumber(grid)
+      this.setState({
+        gridData: grid
+      })
+    }
   }
 
-  componentWillMount(){
-    this.addNumber()
-    this.addNumber()
+  flipGrid(grid) {
+    return grid.map(row => row.reverse())
   }
-  render(){
-    // let board = this.state.board.map((i,id) =>{
-    //    let row = i.map((item, j) => {return <BlockItem num={item}></BlockItem>})
-    //   return <div className="board-row">{row}</div>
-    // })
-    return (
-      <div className="App">
-        <h1>2048</h1>
 
-        <Functionality board={this.state.board} addNumber={this.addNumber} />
-        
+  transpose(grid) {
+    const newGrid = this.blankGrid.map(row => row.slice())
+    for (let i = 0; i < grid.length; i++) {
+      for (let j = 0; j < grid.length; j++) {
+        newGrid[i][j] = grid[j][i]
+      }
+    }
+    return newGrid
+  }
+
+  rightOp() {
+    let copyGrid = this.state.gridData.map(row => row.slice())
+    copyGrid = copyGrid.map(row => this.slideAndCombine(row))
+    this.diffGrid(copyGrid)
+  }
+  leftOp() {
+    let copyGrid = this.state.gridData.map(row => row.slice())
+    copyGrid = this.flipGrid(copyGrid).map(row => this.slideAndCombine(row))
+    copyGrid = this.flipGrid(copyGrid)
+    this.diffGrid(copyGrid)
+  }
+  upOp() {
+    let copyGrid = this.state.gridData.map(row => row.slice())
+    copyGrid = this.transpose(copyGrid)
+    copyGrid = this.flipGrid(copyGrid)
+    copyGrid = copyGrid.map(row => this.slideAndCombine(row))
+    copyGrid = this.flipGrid(copyGrid)
+    copyGrid = this.transpose(copyGrid)
+    this.diffGrid(copyGrid)
+
+  }
+  downOp() {
+    let copyGrid = this.state.gridData.map(row => row.slice())
+    copyGrid = this.transpose(copyGrid)
+    copyGrid = copyGrid.map(row => this.slideAndCombine(row))
+    copyGrid = this.transpose(copyGrid)
+    this.diffGrid(copyGrid)
+  }
+
+  gameOver() {
+    let over = true;
+    this.state.gridData.forEach(row => {
+      row.forEach(data => {
+        if (data === 0) over = false;
+      })
+    })
+    return over
+  }
+  componentDidMount() {
+    this.initGame()
+  }
+
+  render() {
+    let renderOverlay = ''
+    if (this.gameOver()) {
+      renderOverlay = <div className='overlay'>
+        <div className='overlay-msg'>
+          <h4>Game Over</h4>
+          <button onClick={() => this.initGame()}>Restart</button>
+        </div>
       </div>
-    );
+    }
+    return (
+      <HotKeys keyMap={this.map} handlers={this.handlers}>
+        <div className='container'>
+          <div className="centerGrid">
+            <header className="header">
+              <h4 className="title">2048 in React</h4>
+              <div className="score">
+                <span className='score-text'>Score: </span>{this.state.score}
+              </div>
+            </header>
+            <main id="game">
+              <Functionality gridData={this.state.gridData} />
+            </main>
+            {renderOverlay}
+          </div>
+
+        </div>
+
+      </HotKeys>
+    )
   }
 }
 
-export default App;
+export default App
