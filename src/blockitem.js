4e2342bb5eca1c6605d8de9ab27884b651d7d1e4
ali alkaheli <ali.alkaheli96@gmail.com>
2019-05-01 10:36:01 -0700

    Give each block a color depending on the number

--- src/blockitem.js
+++ src/blockitem.js
@@ -1,10 +1,43 @@
-import React from "react";
+import React from 'react'
 import './blockitem.css'
 
-const BlockItem = (props) => {
-    return(
-        <div className="propItem">{props.num}</div>
-    )
+const styles = {
+    extraSmall: {
+        fontSize: '20px',
+        backgroundColor: '#588D9C'
+    },
+    small: {
+        fontSize: '30px',
+        backgroundColor: '#FDA831'
+    },
+    medium: {
+        fontSize: '35px',
+        backgroundColor: "#ED8D8D"
+    },
+    large: {
+        fontSize: '40px',
+        backgroundColor: '#FFC97C'
+    }
 }
+export default class blockItem extends React.Component {
 
-export default BlockItem
+    pickSize(data) {
+        //if number is greater than or equal to 128 
+        //then color dark orange
+        if (data.toString().length > 2) {
+            return styles.small
+        }
+        //if number is greater than or equal to 16 through 64 
+        //color red
+        if (data.toString().length > 1) {
+            return styles.medium
+        }
+        //if the number is 2 then its light orange
+        if (data >= 2) return styles.large
+    }
+    render() {
+        const { data } = this.props
+        const mStyle = this.pickSize(data)
+        return <div className="Box" style={mStyle}>{data > 0 ? data : ''}</div>
+    }
+}
