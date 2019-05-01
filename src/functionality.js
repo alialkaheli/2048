94097cd1b0656e6deb35edcebf59ff434f4019a3
ali alkaheli <ali.alkaheli96@gmail.com>
2019-04-30 21:07:48 -0700

    mapping through each element in the grid into an array called box

--- src/functionality.js
+++ src/functionality.js
@@ -1,70 +1,34 @@
-import React from "react";
+import React from 'react'
+import BlockItem from './blockitem'
 import './functionality.css'
 
-class Functionality extends React.Component{
-    constructor(){
-        super()
-        this.left = this.left.bind(this)
-    }
-
-    left(){
-        for(let i = 0; i < 4; i++){
-            let k = 0
-            for(let j = 1; j < 4; j++){
-                if (
-                  this.props.board[i][k] ===
-                    this.props.board[i][j] &&
-                    this.props.board[i][j] > 0
-                ) {
-                    this.props.board[i][k] *= 2;
-                    this.props.board[i][j] = 0;
-                } else if (this.props.board[i][k] > 0) {
-                         k += 1;
-                } else {
-                    if (this.props.board[i][j] > 0) {
-                        this.props.board[i][
-                          k
-                        ] = this.props.board[i][j];
-                        this.props.board[i][j] = 0;
-                           k += 1;
-                    }
-                }
-            }
-        }
-        
-    }
-    win(){
-        for(let i = 0; i < 4; i++){
-            for(let j = 0; j < 4; j++){
-                if(this.props.board === 2048){
-                    return true
-                }
-            }
-        }
-        return false
-    }
-    render(){
-        return(
-        
-        <div className="whole-board">
-
-            <div className="updown-board">
-                <button className="updown-button">UP</button>
-            </div>
-
-            <div className="mid-board">
-                <button onClick={() => this.left()} className="leftright-button"> LEFT </button>
-                <div>{this.props.board}</div>
-                    <button className="leftright-button"> RIGHT </button>
-            </div>
-
-            <div className="updown-board">
-                <button className="updown-button">DOWN</button>
-            </div>
-        </div>)
-        
-        
+export default class functionality extends React.Component {
+    render() {
+        const { gridData } = this.props
+        //////not as important
+        // const dimension = {
+        //     height: `${4 * 100 + 4}px`,
+        //     width: `${4 * 100 + 4}px`
+        // }
+        let boxes = []
+        //mapping to each element in the array
+        gridData.forEach((rowData, rIndex) => {
+            boxes.push(
+                rowData.map((data, cIndex) => {
+                    return (
+                        //each individual element will contain a color
+                        //in the BlockItem class
+                        //depending on the number 
+                        <div key={`${rIndex}-${cIndex}`}>
+                            <BlockItem id={`${rIndex}-${cIndex}`} data={data} />
+                        </div>
+                    )
+                })
+            )
+        })
+        return (
+          //style={dimension}    add to the div below in the params
+          <div className="Grid">{boxes}</div>
+        );
     }
 }
-
-export default Functionality
